667f8583d1e549d453689abc5d5fda185d9490bd
David Rogers <david_m_rogers@homedepot.com>
2019-10-07 12:29:15 -0400

    better logging output

--- backup.sh
+++ backup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+log_message() {
+    echo ""
+    echo "------------------------------------------------"
+    echo "$*"
+    echo "------------------------------------------------"
+    echo ""
+}
+
 install_dependency() {
 	#TODO make adaptable to multiple OS flavors by parsing the contents of /etc/os-release
 	apk add "$1"
 }
 
-die() { echo "$*" 1>&2 ; exit 1; }
+die() { log_message "$*" 1>&2 ; exit 1; }
 
 need() {
     which "$1" &>/dev/null || install_dependency "$1" || die "Binary '$1' is missing but required"
@@ -18,19 +26,19 @@ create_new_backup() {
 	while [[ ${ATTEMPT} -le ${MAX_ATTEMPTS} && "${STATUS}" != "SUCCESSFUL" ]]
 	do
 		if [[ ${ATTEMPT} -gt 1 ]]; then
-			echo "Waiting ${WAIT_SECONDS} seconds before retrying ..."
+			log_message "Waiting ${WAIT_SECONDS} seconds before retrying ..."
 			sleep ${WAIT_SECONDS}
 		fi
 
-		echo "Attempt number ${ATTEMPT}"
+		log_message "Attempt number ${ATTEMPT}"
 		ATTEMPT=`expr ${ATTEMPT} + 1`
 		DESCRIPTION="On Demand Scheduled Backup: `date +'%Y-%m-%d %H:%M'`"
 
-		echo "Submitting backup ..."
+		log_message "Submitting backup ..."
 		gcloud sql backups create --async --instance ${INSTANCE_ID} --description="${DESCRIPTION}"
 
 		if [[ $? -ne 0 ]]; then
-			echo "WARN: Submit backup job failed!"
+			log_message "WARN: Submit backup job failed!"
 			continue
 		fi
 
@@ -38,39 +46,39 @@ create_new_backup() {
 		gcloud sql operations wait "${JOB_NAME}" --timeout=unlimited
 
 		if [[ $? -ne 0 ]]; then
-			echo "WARN: Create backup job failed!"
+			log_message "WARN: Create backup job failed!"
 			continue
 		fi
 
 		STATUS=$(gcloud sql backups list --instance ${INSTANCE_ID} --filter="type='ON_DEMAND' AND description='${DESCRIPTION}'" --format="value(status)" --limit=1)
 		
 		if [[ "${STATUS}" != "SUCCESSFUL" ]]; then
-			echo "WARN: Backup was not successful, status = ${STATUS}"
+			log_message "WARN: Backup was not successful, status = ${STATUS}"
 		fi
 	done
 
 	if [[ "${STATUS}" == "SUCCESSFUL" ]]; then
-		echo "Backup successful"
+		log_message "Backup successful"
 	else
 		die "ERROR: Exhausted all attempts to backup instance ${INSTANCE_ID}"
 	fi
 }
 
 delete_old_backups() {
-	echo "Listing all On Demand Scheduled Backups"
+	log_message "Listing all On Demand Scheduled Backups"
 	gcloud sql backups list --instance ${INSTANCE_ID} --sort-by="~startTime" --filter="type='ON_DEMAND' AND description~'On Demand' AND status!='RUNNING'" --format="table[box](id, type, status, startTime, endTime, description)"
 
 	LINE_NUMBER=`expr ${BACKUPS_TO_KEEP} + 1`
 	BACKUP_IDS=`gcloud sql backups list --instance ${INSTANCE_ID} --sort-by="~startTime" --filter="type='ON_DEMAND' AND description~'On Demand' AND status!='RUNNING'" --format="value(id)" | tail -n +${LINE_NUMBER} | cut -f 1`
 
 	if [[ -n "${BACKUP_IDS}" ]]; then
-		echo "Number of backups to keep: ${BACKUPS_TO_KEEP}, deleting extras ..."
-		for BACKUP_ID in `echo "${BACKUP_IDS}"`
+		log_message "Number of backups to keep: ${BACKUPS_TO_KEEP}, deleting extras ..."
+		for BACKUP_ID in `log_message "${BACKUP_IDS}"`
 		do
 			gcloud sql backups delete ${BACKUP_ID} --instance ${INSTANCE_ID} --quiet
 
 			if [[ $? -ne 0 ]]; then
-				echo "WARN: Delete of backup ID ${BACKUP_ID} failed"
+				log_message "WARN: Delete of backup ID ${BACKUP_ID} failed"
 			fi
 		done
 	fi
